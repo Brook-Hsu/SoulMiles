f9730ec329dc0ce2317902d32afff7e78ba09ba9
Leestar <st973707@gmail.com>
2025-12-01 04:57:44 +0900

    [fix] 網頁圖片

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,14 @@ import SessionProvider from '../components/SessionProvider'
 export const metadata = {
   title: 'SoulMiles - 迷霧中的靈魂之旅',
   description: 'Q版暗黑哥德 x 迷霧尋寶主題的旅遊足跡應用',
+  icons: {
+    icon: [
+      { url: '/images/routes/soulmiles.jpg', sizes: 'any' },
+    ],
+    apple: [
+      { url: '/images/routes/soulmiles.jpg', sizes: '1024x1024', type: 'image/jpeg' },
+    ],
+  },
 }
 
 export default function RootLayout({ children }) {
